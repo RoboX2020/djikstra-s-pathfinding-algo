55288eba0fbc343df3fe1baa0ff24a040fa6e19d
tkC0ding <tkjagani@gmail.com>
2026-04-17 16:04:10 -0700

    input grid generation code complete

--- inp.py
+++ inp.py
@@ -2,10 +2,38 @@ import numpy as np
 
 def grid(n_rows:int, n_cols:int, num_obstacles:int, num_priorities:int):
     a = np.zeros((n_rows,n_cols))
-    start_index = (np.random.randint(0, n_rows-1),np.random.randint(0, n_rows-1))
+    b = list(range(0, n_rows))
+    c = list(range(0, n_cols))
+    
+    start_index = (np.random.choice(b), np.random.choice(c))
+    
+    b.remove(start_index[0])
+    c.remove(start_index[1])
 
-    end_index = start_index
-    while end_index == start_index:
-        end_index = (np.random.randint(0, n_rows-1),np.random.randint(0, n_rows-1))
-    
-    
+    end_index = (np.random.choice(b), np.random.choice(c))
+
+    b.remove(end_index[0])
+    c.remove(end_index[1])
+
+    a[start_index[0], start_index[1]] = 1
+    a[end_index[0], end_index[1]] = 2
+
+    for i in range(num_obstacles):
+        point = (np.random.choice(b), np.random.choice(c))
+
+        b.remove(point[0])
+        c.remove(point[1])
+
+        a[point[0], point[1]] = 3
+
+    for i in range(num_priorities):
+        point = (np.random.choice(b), np.random.choice(c))
+
+        b.remove(point[0])
+        c.remove(point[1])
+
+        a[point[0], point[1]] = 4
+
+    return a
+
+print(grid(10, 10, 2, 1))
