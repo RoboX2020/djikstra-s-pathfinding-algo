682b69d0953689068b126ec17caa22188dc392e2
tkC0ding <tkjagani@gmail.com>
2026-04-17 15:46:03 -0700

    added start and end index generation

---
+++ inp.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+def grid(n_rows:int, n_cols:int, num_obstacles:int, num_priorities:int):
+    a = np.zeros((n_rows,n_cols))
+    start_index = (np.random.randint(0, n_rows-1),np.random.randint(0, n_rows-1))
+    end_index = (np.random.randint(0, n_rows-1),np.random.randint(0, n_rows-1))
